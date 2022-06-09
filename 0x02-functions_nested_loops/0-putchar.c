11c74266b4538fa248fe1a3687b454846fe3ab71
Isaac <105236079+Isaac-sei@users.noreply.github.com>
2022-06-09 00:15:44 -0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,37 +1,17 @@
 #include "main.h"
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
-int main(void)
-  
+int main(void) 
 {
-  
   char *sh = "_putchar";
-  
-
-  
   while (*sh)
-    
     {
-      
       _putchar(*sh);
-      
-      sh++;
-      
+      sh++; 
     }
-  
   _putchar('\n');
-  
-
-  
-  return (0);
-  
+  return (0)
 }
