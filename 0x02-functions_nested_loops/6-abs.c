0a31856a13055f17f7af802de838f8cb8d47ef0a
Isaac <105236079+Isaac-sei@users.noreply.github.com>
2022-06-09 01:41:25 -0200

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,39 +1,21 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-
- * _abs - Computes the absolute value of an integer.
-
- * @c: The number to be computed.
-
- *
-
- * Return: Absolute value of number or zero
-
- */
-
+  * _abs - Computes the absolute value of an integer.
+  * @c: The number to be computed.
+  *
+  * Return: Absolute value of number or zero
+  */
 int _abs(int c)
-  
 {
-  
-  if (c < 0)
-    
-    {
-      
-      int abs_val;
-      
+	if (c < 0)
+	{
+		int abs_val;
 
-      
-      abs_val = c * -1;
-      
+		abs_val = c * -1;
 
-      
-      return (abs_val);
-      
-    }
-  
+		return (abs_val);
+	}
 
-  
-  return (c);
-  
+	return (c);
 }
